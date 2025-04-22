48f87e0fec327877f91ccf8fcd485826225180db
Guenzler <148702094+Guenzler@users.noreply.github.com>
2025-04-22 15:55:44 +0200

    add support for strings with punctuation, write tests and refactor

--- lib/eguen_palindrome.rb
+++ lib/eguen_palindrome.rb
@@ -7,13 +7,13 @@ class String
   # Returns true for a palindrome, false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
-  end
+  end 
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.downcase
+      scan(/[a-z]/i).join.downcase
     end
 end
 
--- test/test_eguen_palindrome.rb
+++ test/test_eguen_palindrome.rb
@@ -5,18 +5,19 @@ require "test_helper"
 class TestEguenPalindrome < Minitest::Test
 
   def test_non_palindrome
-    assert !"apple".palindrome?
+    refute "apple".palindrome?
   end
 
   def test_literal_palindrome
     assert "racecar".palindrome?
   end
 
-  def test_palindrome_with_punctuation
-    skip
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
   end
 
-  def test_mixed_case_palindrome
-    skip
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
   end
+
 end
